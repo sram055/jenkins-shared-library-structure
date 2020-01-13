88c0b0112cbcb328aee16fc71259f9892da2945c
sram055 <sreedevi.ramineni@gmail.com>
2020-01-13 06:58:48 +0000

    added mycode

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,20 @@
 
+#def call(Map stageParams) {
+ 
+#    checkout([
+#        $class: 'GitSCM',
+#        branches: [[name:  stageParams.branch ]],
+#        userRemoteConfigs: [[ url: stageParams.url ]]
+#    ])
+#  }
+
 def call(Map stageParams) {
  
     checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+	$class: 'GitSCM',
+	 branches: [[name: '*/master']],
+	 doGenerateSubmoduleConfigurations: false,
+	 extensions: [],
+	 submoduleCfg: [],
+	 userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/sram055/jenkins-shared-library-structure.git']]])
   }
